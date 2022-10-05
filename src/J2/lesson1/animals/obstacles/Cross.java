7179bafe7337ec1b978a233ed74c3a13b57d3db4
Vadim <95380405+VadimSTV@users.noreply.github.com>
2022-10-05 21:06:28 +0300

    Update Cross.java

--- src/J2/lesson1/animals/obstacles/Cross.java
+++ src/J2/lesson1/animals/obstacles/Cross.java
@@ -1,10 +1,10 @@
 package J2.lesson1.animals.obstacles;
 import J2.lesson1.animals.Participant.Participant;
 
-//Класс наследования Cross родителя Obstacle
+//Класс наследования Obstacle подкласса Cross
 public class Cross extends Obstacle {
 
-    private int length;
+    private int length; //метод length(длина кросса)
 
     public Cross(int length) {
         this.length = length;
